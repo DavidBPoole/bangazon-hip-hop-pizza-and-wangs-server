453e48a4fe34aaf642eb72d2a49552f02af30933
David Poole <dbpoole7@gmail.com>
2024-01-20 00:16:06 -0600

    completed functionality to close orders and create a revenue node that captures closed order data

--- bangazonapi/models/revenue.py
+++ bangazonapi/models/revenue.py
@@ -7,5 +7,5 @@ class Revenue(models.Model):
   date = models.DateTimeField(default=timezone.now)
   payment = models.CharField(max_length=55)
   subtotal = models.DecimalField(max_digits=7, decimal_places=2)
-  total = models.DecimalField(max_digits=7, decimal_places=2)
   tip = models.DecimalField(max_digits=7, decimal_places=2)
+  total = models.DecimalField(max_digits=7, decimal_places=2)
